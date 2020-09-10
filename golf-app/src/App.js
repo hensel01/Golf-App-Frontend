b46061ec2f72ddb4aabe5d598c256b053f953b27
hensel01 <eliotrhenson@gmail.com>
2020-09-09 22:40:04 -0400

    import components

--- golf-app/src/App.js
+++ golf-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './Containers/Homepage'
+import Leaderboard from './Containers/Leaderboard'
 
 function App() {
   return (
---
+++ golf-app/src/Containers/Leaderboard.js
@@ -0,0 +1,12 @@
+class Leaderboard extends React.Component {
+    render() {
+    
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Leaderboard;
